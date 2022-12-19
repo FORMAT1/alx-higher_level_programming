e3dde560beaaf28e7efe3c4dc612ae758339e3b9
FORMAT1 <olayiwolafm@gmail.com>
2022-12-19 21:53:06 +0000

    mode changed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
-        print("{:d}\n".format(value))
-        return True
+        print("{:d}".format(value))
     except:
         return False
+    return True

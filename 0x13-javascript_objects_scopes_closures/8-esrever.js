6479c0efe51beb9b92f03094133450c63d5ac7d9
FORMAT1 <olayiwolafm@gmail.com>
2023-03-15 01:36:54 +0000

    fixing semistandard error

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-//A function that returns the reversed version of a list
-
+// A function that returns the reversed version of a list
 exports.esrever = function (list) {
-  
-  let rlist = [];
+  const rlist = [];
   while (list.length > 0) {
     rlist.push(list.pop());
   }
